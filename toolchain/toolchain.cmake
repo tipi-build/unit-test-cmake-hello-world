6eda1267b7b2e3ea50f9e8880774bfb1bbd45e06
Lambourl <65184664+Lambourl@users.noreply.github.com>
2024-04-16 13:56:24 +0200

    first commit

---
+++ toolchain/toolchain.cmake
@@ -0,0 +1,26 @@
+find_program(CMAKE_C_COMPILER clang)
+find_program(CMAKE_CXX_COMPILER clang++)
+
+set(
+    CMAKE_C_COMPILER
+    "${CMAKE_C_COMPILER}"
+    CACHE
+    STRING
+    "C compiler"
+    FORCE
+)
+
+set(
+    CMAKE_CXX_COMPILER
+    "${CMAKE_CXX_COMPILER}"
+    CACHE
+    STRING
+    "C++ compiler"
+    FORCE
+)
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "All binaries have to be compiled with fPIC" FORCE)
+set(CMAKE_CROSSCOMPILING FALSE)
+set(CMAKE_GENERATOR "Ninja" CACHE INTERNAL "" FORCE)
+set(CMAKE_CXX_STANDARD "17" CACHE STRING "" FORCE)
+#c3380851-eb59-4c11-92b9-3125aa4a6c64
